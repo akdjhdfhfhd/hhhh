7bbad44074553adfb69f65c1f6c179b50c77afc2
akdjhdfhfhd <125655757+akdjhdfhfhd@users.noreply.github.com>
2023-02-26 19:57:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,35 @@ AUTO_DOWNLOADS_CLEAR = getenv("AUTO_DOWNLOADS_CLEAR", "True")
 PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
 
 YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "5"))
-TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "3"))
+TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "3"))from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "5000"))
+
+OWNER_ID = int(getenv("OWNER_ID"))
+
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/22394c2ebaf52272be4da.jpg")
+
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/22394c2ebaf52272be4da.jpg")
+
+SESSION = getenv("SESSION", None)
+
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ah_9_v")
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ah07v")
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5656828413").split()))
+
+FAILED = "https://te.legra.ph/file/22394c2ebaf52272be4da.jpg"
 
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
